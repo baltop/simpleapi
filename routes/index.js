2cd95ade93d6c4a995a51867ee8731376c8fc922
baltop <baltop@gmail.com>
2021-11-17 16:25:33 +0900

    openlayer test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const knex = require('../db/knex');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/pp', async function(req, res, next) {
 
     const ccList = await knex.select().table('public.client');
     console.log(ccList);
