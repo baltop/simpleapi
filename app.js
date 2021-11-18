0a9d69a64253f6e6e3df80ff37bf8e05ba1fc595
baltop <baltop@gmail.com>
2021-11-18 09:07:35 +0900

    openlayers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var hbs = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var session = require('express-session');
 var logger = require('morgan')
 
@@ -33,7 +33,14 @@ if (process.env.NODE_ENV == 'production') {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+var hbs = exphbs.create({
+  extname: '.hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 // app.engine('mst', mustache(VIEWS_PATH + '/partials', '.mst'));
 app.use(logger('dev'));
 app.use(express.json());
